09de4d67fd3e05c9348b22792be5d11bfe576fd7
joaodanielkm <joaodanielkm@gmail.com>
2022-08-08 21:13:17 -0300

    VERSAO 1

---
+++ ConsultaCEP/FrmConsultaCEP.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsultaCEP
+{
+    public partial class FrmConsultaCEP : Form
+    {
+        public FrmConsultaCEP()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
